d2eb0f909788b3e3ff09cc60ed96b5befdd21823
gerarddo <germijares280995@hotmail.com>
2018-01-16 11:50:09 -0600

    now panelists modal open with click on panelists info

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -2,27 +2,28 @@
 
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var panelistsModal = document.getElementById('panelists-modal');
+// var workshopsModal = document.getElementById('panelists-modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var openPanelistsModalBtn = document.getElementById("open-panelists-modal-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closePanelistsModalBtn = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+// When the user clicks the open button, open the modal 
+openPanelistsModalBtn.onclick = function() {
+    panelistsModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// When the user clicks the close button, close the modal
+closePanelistsModalBtn.onclick = function() {
+    panelistsModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == panelistsModal) {
+        panelistsModal.style.display = "none";
     }
 }
