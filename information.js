b9c884b9b35ccafb47b28b218d47b9a161f0486f
gerarddo <germijares280995@hotmail.com>
2018-01-16 18:32:31 -0600

    bleh

--- information.js
+++ information.js
@@ -52,7 +52,7 @@ router.visits = [
 		],
 		image: "http://viakem.com/wp-content/uploads/2017/07/Logo-para-FB-800x800.png"
 	}, {
-		name: "Cuauhtémoc Moctezuma/Heineken",
+		name: "Cuauhtémoc Moctezuma / Heineken",
 		body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		schedules: [
 			"Someday from 8:00 to 12:00 (Group 1)"
