d1ab3b930cca2824dc493ee974e38278a462731d
gerarddo <germijares280995@hotmail.com>
2018-01-24 18:52:53 -0600

    weather bundle is complete, also fixed minor bugs with navbar

---
+++ public/scripts/foreigners-map.js
@@ -0,0 +1,1 @@
+foreigners-map.js
--- routes/index.js
+++ routes/index.js
@@ -21,15 +21,16 @@ router.get("/foreigners", function(req, res){
 	let units = config.units;
 	let lang = config.lang
 	let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}&lang=${lang}`
-	var forecast = [];
+
 
 	request(url, function (err, response, body) {
 	  	if(err){
 	   		console.log('error:', error);
 	  	} else {
-	    	forecast = modifyForecast(JSON.parse(body).list)
-	    	console.log(forecast)
-	        res.render("foreigners", {homepage: false, forecast: forecast})
+	    	let forecast = modifyForecast(JSON.parse(body).list)
+	    	let today = JSON.parse(body).list[0]
+	    	console.log(today)
+	        res.render("foreigners", {homepage: false, forecast: forecast, today: today})
 	  	}
 	});
 
@@ -86,34 +87,80 @@ function modifyForecast(forecast){
 	var lastWeekdayIndex = -1
 	var index = -1
 
+	var temp_min = [[],[],[],[],[],[],[],[]]
+	var temp_max = [[],[],[],[],[],[],[],[]]
+	var icon_class = [[],[],[],[],[],[],[],[]]
+
+	var defTemp_min = []
+	var	defTemp_max = []
+	var	defIcon_class = []
 
 	forecast.forEach((weather) => {
 		let date = new Date(weather.dt_txt + " GMT+06:00")
 		let weekdayIndex = date.getDay()
 
-	    let newWeatherObj = {
-	    	temp: weather.main.temp,
-	    	temp_min: weather.main.temp_min,
-	    	temp_max: weather.main.temp_max,
-	    	humidity: weather.main.humidity,
-	    	icon_class: getIconUrl(weather),
-	    	weekday: weekdays[weekdayIndex],
-	    	hour: date.getHours()
-	    }
-
-
 	    if(weekdayIndex === lastWeekdayIndex ){
-	    	weekForecast[index].push(newWeatherObj)
+	    	temp_min[index].push(weather.main.temp_min)
+	    	temp_max[index].push(weather.main.temp_max)
+	    	icon_class[index].push({
+	    		value: getIconUrl(weather),
+	    		hour: date.getHours(),
+	    		weekday: weekdays[weekdayIndex]
+	    	})
 	    } else {
+
 	    	lastWeekdayIndex = weekdayIndex
 	    	index++
-	    	weekForecast[index].push(newWeatherObj)
-	    }
 
+	    	temp_min[index].push(weather.main.temp_min)
+	    	temp_max[index].push(weather.main.temp_max)
+	    	icon_class[index].push({
+	    		value: getIconUrl(weather),
+	    		hour: date.getHours(),
+	    		weekday: weekdays[weekdayIndex]
+	    	})
+	    }
 	})
 
-	return weekForecast
 
+	for(var i = 0; i < 6; i++){
+
+		defTemp_min[i] = Math.min(...temp_min[i])
+		defTemp_max[i] = Math.max(...temp_max[i])
+
+		let foundClass = false;
+
+		icon_class.forEach((i_class) => {
+			if(i_class.hour === 12){
+				defIcon_class[i] = i_class.value
+				foundClass = true;
+			}
+		})
+
+		if(!foundClass){
+			defIcon_class[i] = icon_class[i][icon_class[i].length - 1]
+		}
+
+	}
+
+	// console.log(defTemp_min)
+	// console.log(defTemp_max)
+	// console.log(defIcon_class)
+
+	for(var i = 0; i < 6; i++){
+		weekForecast[i] = {
+			weekday: defIcon_class[i].weekday,
+			temp_min: defTemp_min[i],
+			temp_max: defTemp_max[i],
+			icon_class: defIcon_class[i].value
+		}
+	}
+
+
+
+
+
+	return weekForecast
 }
 
 
