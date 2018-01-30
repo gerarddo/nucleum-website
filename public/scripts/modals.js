bfb625d4a282da02d16aa48d21337d0af5074d51
gerarddo <germijares280995@hotmail.com>
2018-01-29 18:41:07 -0600

    fixed modals view and changed the way of closing the modals

--- public/scripts/modals.js
+++ public/scripts/modals.js
@@ -1,61 +1,83 @@
 
-var panelistsModal = document.getElementById('panelists-modal');
-var openPanelistsModalBtn = document.getElementById("open-panelists-modal");
-var closePanelistsModalBtn = document.getElementsByClassName("close-panelists-modal")[0];
+// Panelists
 
-openPanelistsModalBtn.onclick = function() { panelistsModal.style.display = "block"; }
-closePanelistsModalBtn.onclick = function() { panelistsModal.style.display = "none"; }
+    var panelistsModal = document.getElementById('panelists-modal');
+    var openPanelistsModalBtn = document.getElementById("open-panelists-modal");
+    var closePanelistsModalBtn = $(".close-panelists-modal")[0];
 
-// When the user clicks anywhere outside of the modal, close it
-$(".container-body").click((event) => {
-    if (event.target === panelistsModal) {
-        panelistsModal.style.display = "none";
-        console.log(event.target)
+    openPanelistsModalBtn.onclick = function() { panelistsModal.style.display = "block"; }
+    closePanelistsModalBtn.onclick = function() { panelistsModal.style.display = "none"; }  
+
+    // When the user clicks anywhere outside of the modal, close it
+    $(".container-body").click((event) => {
+        if (event.target === panelistsModal) {
+            panelistsModal.style.display = "none";
+            console.log(event.target)
+        }
+    })
+
+// Business
+
+    var businessModal = document.getElementById('business-modal');
+    var openBusinessModalBtn = document.getElementsByClassName("open-business-modal");
+    var closeBusinessModalBtn = $(".close-business-modal")[0];
+
+
+
+    for(var i = 0; i < openBusinessModalBtn.length; i++){
+        openBusinessModalBtn[i].onclick = function() { 
+            businessModal.style.display = "block"; 
+            // console.log("het")
+        }
     }
-})
 
+    closeBusinessModalBtn.onclick = function() { businessModal.style.display = "none"; }  
 
+    // When the user clicks anywhere outside of the modal, close it
+    $(".container-body").click((event) => {
+        if (event.target === businessModal) {
+            businessModal.style.display = "none";
+            console.log(event.target)
+        }
+    })
 
+// Workshops
 
+    var workshopsModal = document.getElementById('workshops-modal');
+    var openWorkshopsModalBtn = document.getElementsByClassName("open-workshops-modal");
+    var closeWorkshopsModalBtn = document.getElementsByClassName("close-workshops-modal")[0];
 
-
-
-
-var workshopsModal = document.getElementById('workshops-modal');
-var openWorkshopsModalBtn = document.getElementsByClassName("open-workshops-modal");
-var closeWorkshopsModalBtn = document.getElementsByClassName("close-workshops-modal")[0];
-
-for(var i = 0; i < openWorkshopsModalBtn.length; i++){
-	openWorkshopsModalBtn[i].onclick = function() { 
-		workshopsModal.style.display = "block"; 
-	}
-}
-
-closeWorkshopsModalBtn.onclick = function() { workshopsModal.style.display = "none"; }
-
-// When the user clicks anywhere outside of the modal, close it
-$(".container-body").click((event) => {
-    if (event.target == workshopsModal) {
-        workshopsModal.style.display = "none";
+    for(var i = 0; i < openWorkshopsModalBtn.length; i++){
+    	openWorkshopsModalBtn[i].onclick = function() { 
+    		workshopsModal.style.display = "block"; 
+    	}
     }
-})
 
+    closeWorkshopsModalBtn.onclick = function() { workshopsModal.style.display = "none"; }
 
+    // When the user clicks anywhere outside of the modal, close it
+    $(".container-body").click((event) => {
+        if (event.target == workshopsModal) {
+            workshopsModal.style.display = "none";
+        }
+    })
 
-var visitsModal = document.getElementById('visits-modal');
-var openVisitsModalBtn = document.getElementsByClassName("open-visits-modal");
-var closeVisitsModalBtn = document.getElementsByClassName("close-visits-modal")[0];
+// Visits
 
-for(var i = 0; i < openVisitsModalBtn.length; i++){
-	openVisitsModalBtn[i].onclick = function() { 
-		visitsModal.style.display = "block"; 
-	}
-}
-closeVisitsModalBtn.onclick = function() { visitsModal.style.display = "none"; }
+    var visitsModal = document.getElementById('visits-modal');
+    var openVisitsModalBtn = document.getElementsByClassName("open-visits-modal");
+    var closeVisitsModalBtn = document.getElementsByClassName("close-visits-modal")[0];
 
-// When the user clicks anywhere outside of the modal, close it
-$(".container-body").click((event) => {
-    if (event.target == visitsModal) {
-        visitsModal.style.display = "none";
+    for(var i = 0; i < openVisitsModalBtn.length; i++){
+    	openVisitsModalBtn[i].onclick = function() { 
+    		visitsModal.style.display = "block"; 
+    	}
     }
-})
+    closeVisitsModalBtn.onclick = function() { visitsModal.style.display = "none"; }
+
+    // When the user clicks anywhere outside of the modal, close it
+    $(".container-body").click((event) => {
+        if (event.target == visitsModal) {
+            visitsModal.style.display = "none";
+        }
+    })
